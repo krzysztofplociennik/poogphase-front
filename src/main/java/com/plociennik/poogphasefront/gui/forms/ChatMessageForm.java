c858c620f188455d5581a6cd07273c055e496731
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2021-03-30 09:02:00 +0100

    Fixed chat messages timestamps

--- src/main/java/com/plociennik/poogphasefront/gui/forms/ChatMessageForm.java
+++ src/main/java/com/plociennik/poogphasefront/gui/forms/ChatMessageForm.java
@@ -8,6 +8,10 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class ChatMessageForm extends FormLayout {
     private ApiClient apiClient;
@@ -23,11 +27,22 @@ public class ChatMessageForm extends FormLayout {
                 .findFirst().get();
 
         Paragraph details = new Paragraph(author.getUsername() +
+                ", " +
+                addDateIfNotToday(chatMessageDto) +
                 String.format(" %02d:%02d", chatMessageDto.getDateTime().getHour(), chatMessageDto.getDateTime().getMinute()));
         Paragraph content = new Paragraph(chatMessageDto.getContent());
         Paragraph separator = new Paragraph("----------------");
 
         VerticalLayout verticalLayout = new VerticalLayout(details, content, separator);
         add(verticalLayout);
+    }
+
+    public String addDateIfNotToday(ChatMessageDto message) {
+        if (Period.between(message.getDateTime().toLocalDate(), LocalDate.now()).getDays() > 0) {
+            return " " + message.getDateTime().getDayOfMonth() +
+                    " " + message.getDateTime().getMonth().getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + ", ";
+        } else {
+            return "";
+        }
     }
 }
---
+++ src/test/java/com/plociennik/poogphasefront/other/VariousTests.java
@@ -0,0 +1,44 @@
+package com.plociennik.poogphasefront.other;
+
+import com.plociennik.poogphasefront.model.ChatMessageDto;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VariousTests {
+    public static void main(String[] args) {
+
+        List<ChatMessageDto> chatLog = new ArrayList<>();
+
+        ChatMessageDto message = new ChatMessageDto();
+        message.setDateTime(LocalDateTime.of(LocalDate.of(2021, 3, 28), LocalTime.of(17, 22)));
+        message.setContent("Paul: hey there Mark!");
+        ChatMessageDto message2 = new ChatMessageDto();
+        message2.setDateTime(LocalDateTime.of(LocalDate.of(2021, 3, 29), LocalTime.of(9, 1)));
+        message2.setContent("Mark: oh hey, sorry I was busy");
+        ChatMessageDto message3 = new ChatMessageDto();
+        message3.setDateTime(LocalDateTime.of(LocalDate.of(2021, 3, 29), LocalTime.of(9, 2)));
+        message3.setContent("Mark: how are you?");
+        ChatMessageDto message4 = new ChatMessageDto();
+        message4.setDateTime(LocalDateTime.of(LocalDate.of(2021, 3, 29), LocalTime.of(9, 10)));
+        message4.setContent("Paul: im super good, thanks!");
+
+        chatLog.addAll(Arrays.asList(message, message2, message3, message4));
+    }
+
+    public static void chatTimeStamp(ChatMessageDto chatMessageDto) {
+        if (Period.between(chatMessageDto.getDateTime().toLocalDate(), LocalDate.now()).getDays() > 0) {
+            System.out.println(chatMessageDto.getDateTime().getDayOfMonth() +
+                    " " + chatMessageDto.getDateTime().getMonth() +
+                    " " + chatMessageDto.getContent());
+        } else {
+            System.out.println(chatMessageDto.getDateTime().getHour() +
+                    ":" + chatMessageDto.getDateTime().getMinute() +
+                    " " + chatMessageDto.getContent());
+        }
+    }
+}
